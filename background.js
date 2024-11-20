9f8c0d37094d672b2c03bf8ed610ed9f4c682b30
belanovic <goranbelanovic@yahoo.com>
2024-11-20 05:09:21 +0100

    msg

--- background.js
+++ background.js
@@ -5,5 +5,64 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "checkFacts" && request.tabId) {
     chrome.tabs.sendMessage(request.tabId, { action: "checkFacts" });
   }
- 
-});
+  if (request.action === "arrangeArticle" && request.tabId) {
+     chrome.storage.local.get('textArticle', (result) => {
+      if(chrome.runtime.lastError) {
+        console.error('Error retrieving data:', chrome.runtime.lastError.message);
+      } else if (result.textArticle) {
+        arrangeArticle(result.textArticle, request.tabId);
+      }
+        
+    })
+  }
+})
+
+
+
+
+
+
+
+async function arrangeArticle(textArticle, x) {
+  async function sendText() {
+      
+      try {
+          const response = await fetch(/* 'http://localhost:3000/chatGPT/article'  */'https://grammar-backend.onrender.com/chatGPT/article', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json'
+                },
+              body: JSON.stringify({
+                  text: textArticle,
+                  prompt: "Среди овај новински текст на српском, у стилу чланака на сајту Радио-телевизије Србије. Немој да уклониш регуларан текст, само исправи шта је потребно. Уклони сувишне размаке између речи, направи пасусе, исправи словне грешке, али немој да изоставиш ниједан део регуларног текста. Имена и презимена треба само да почињу великим словом. Текст треба да буде на ћирилици. Измисли пет занимљивих наслова на основу текста. Ево текста: ",
+                  description: 'Ви сте асистент за сређивање новинских текстова на српском језику за сајт Радио-телевизије Србије'
+              })
+          });
+          const responseBody = await response.text();
+          return responseBody
+      } catch (error) {
+          alert(error.message);
+          return
+      }
+  }
+
+  let answer = await sendText();
+
+  answer = JSON.parse(answer);
+  
+  
+  if((!answer) || (answer == []) || (answer == '')) {
+      alert('ChatGPT није средио текст');
+      return
+  }
+  chrome.storage.local.set({textArticle: answer});
+  console.log(answer)
+  chrome.runtime.sendMessage({ type: 'textArranged', payload: 'Hello from the background!' }, (response) => {
+    if (chrome.runtime.lastError) {
+      console.error('Error:', chrome.runtime.lastError.message);
+    } else {
+      console.log('Response from popup:', response);
+    }
+  });
+  return answer;
+}
--- popup.js
+++ popup.js
@@ -2,9 +2,6 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
-
-
-
     const tab = document.getElementsByClassName('tab');
     const operation = document.getElementsByClassName('operation');
 
@@ -41,9 +38,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const btnFacts = document.getElementsByClassName('facts')[0];
     const btnArticleSend = document.getElementsByClassName('btnArticleSend')[0];
     const textArticle = document.getElementsByClassName('textArticle')[0];
-    textArticle.value = localStorage.getItem('textArticle');
-    btnArticleSend.addEventListener('click', (e) => arrangeArticle(textArticle))
+
+    chrome.storage.local.get('textArticle', (result) => textArticle.value = result.textArticle);
+    
+
     const btnArticleStrip = document.getElementsByClassName('btnArticleStrip')[0];
+
     btnArticleStrip.addEventListener('click', () => {
         textArticle.value = removeLargeHexadecimalParts(textArticle.value);
         function removeLargeHexadecimalParts(text) {
@@ -52,9 +52,10 @@ document.addEventListener("DOMContentLoaded", function() {
             return strippedText
         }
     })
-    textArticle.addEventListener('change', (e) => {
-        localStorage.setItem('textArticle', e.target.value);
+    textArticle.addEventListener('input', (e) => {
+        chrome.storage.local.set({textArticle: e.target.value});
     })
+
     
     btnSpelling.addEventListener('click', function() {
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
@@ -66,42 +67,17 @@ document.addEventListener("DOMContentLoaded", function() {
             chrome.runtime.sendMessage({ action: "checkFacts", tabId: tabs[0].id });
         });
     })
+    btnArticleSend.addEventListener('click', function() {
+        if(!window.confirm(`Да ли желите да пошаљете текст ChatGPT-ju?`)) return;
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            chrome.runtime.sendMessage({ action: "arrangeArticle", tabId: tabs[0].id });
+        });
+    })
+    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+        if (message.type === 'textArranged') {
+            chrome.storage.local.get('textArticle', (result) => textArticle.value = result.textArticle);
+          }
+        sendResponse({ status: 'Message received by popup' });
+    })
 
 }) 
-
-async function arrangeArticle(textArticle) {
-    async function sendText() {
-        
-        if(!window.confirm(`Да ли желите да пошаљете текст ChatGPT-ju?`)) return;
-        try {
-            const response = await fetch(/* 'http://localhost:3000/chatGPT/article'  */'https://grammar-backend.onrender.com/chatGPT/article', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                  },
-                body: JSON.stringify({
-                    text: textArticle.value,
-                    prompt: "Среди овај новински текст на српском, у стилу чланака на сајту Радио-телевизије Србије. Немој да уклониш регуларан текст, само исправи шта је потребно. Уклони сувишне размаке између речи, направи пасусе, исправи словне грешке, али немој да изоставиш ниједан део регуларног текста. Имена и презимена треба само да почињу великим словом. Текст треба да буде на ћирилици. Измисли пет занимљивих наслова на основу текста. Ево текста: ",
-                    description: 'Ви сте асистент за сређивање новинских текстова на српском језику за сајт Радио-телевизије Србије'
-                })
-            });
-            const responseBody = await response.text();
-            return responseBody
-        } catch (error) {
-            alert(error.message);
-            return
-        }
-    }
-
-    let answer = await sendText();
-
-    answer = JSON.parse(answer);
-    
-    
-    if((!answer) || (answer == []) || (answer == '')) {
-        alert('ChatGPT није средио текст');
-        return
-    };
-    textArticle.value = answer;
-}
-
